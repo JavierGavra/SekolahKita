293ed9eaaff6089449e47b91316e3b55e657f49d
Lenovo Gavra <gavrajavier01@gmail.com>
2026-01-17 13:51:22 +0700

    refactor: [JG] remove supabase service

--- lib/core/bloc/provider.dart
+++ lib/core/bloc/provider.dart
@@ -1,12 +1,10 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sekolah_kita/features/auth/bloc/auth_bloc.dart';
 import 'package:sekolah_kita/features/course/cubit/course/course_cubit.dart';
 import 'package:sekolah_kita/features/profile/bloc/profile_bloc.dart';
 
 class Provider {
   static List<BlocProvider> get providers {
     return [
-      BlocProvider<AuthBloc>(create: (context) => AuthBloc()),
       BlocProvider<CourseCubit>(create: (context) => CourseCubit()),
       BlocProvider<ProfileBloc>(create: (context) => ProfileBloc()),
     ];
